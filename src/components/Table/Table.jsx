e86e84fe64a8efbc7a0340a87bdeee8b39eaa4f0
Katherinne <cateolaya3@gmail.com>
2025-07-18 16:30:24 -0500

    Actualizado

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,79 +6,45 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { makeStyles } from '@mui/material';
 import './Table.css';
-//import React, {useEffect, useState} from 'react'
-
-//import axios from 'axios';
-
-function createData(Hamburguesa, Id, Ventas, Precio, Estado) {
-  return { Hamburguesa, Id, Ventas, Precio, Estado };
-}
-
-/* ESTO TENGO QUE CAMBIARLO EN LA BASE DE DATOS */
-const rows = [
-  createData('El Chato', 1, 8, 32, 'En Stock'),
-  createData('El Amado', 2, 10, 40, 'Sin Stock'),
-  createData('El Parrillero', 3, 11.2, 38, 'En Stock'),
-  
-];
-
-const makeStyle=(Status)=>{
-    if(Status == 'En Stock')
-    {
-        return {
-            background : ('rgb(145 254 159 / 47%'),
-            color : 'green'
-        }
-    }
-    else if(Status == 'Sin Stock')
-    {
-    return {
-        background : '#ffadad8f',
-        color : 'red'
-    }
-}
-    else 
-    {
-    return {
-        background : '#59bfff',
-        color : 'white'
-    }
-}
-}
 
 export default function BasicTable() {
+  const [rows, setRows] = useState([]);
+
+useEffect(() => {
+  fetch('http://localhost:3001/ordenes-recientes')
+    .then(res => res.json())
+    .then(data => {
+      console.log("Datos recibidos del backend:", data); // <---- AGREGA ESTO
+      setRows(data);
+    });
+}, []);
+
+
   return (
     <div className="Table">
       <h3>Ordenes recientes</h3>
-
-      <TableContainer component={Paper}
-      style={{boxShadow : '0px 13px 20px 0px #80808029'}}>
+      <TableContainer component={Paper} style={{ boxShadow: '0px 13px 20px 0px #80808029' }}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
-  <TableRow>
-    <TableCell>Producto</TableCell>
-    <TableCell align="left">Id</TableCell>
-    <TableCell align="left">Ventas</TableCell>
-    <TableCell align="left">Precio&nbsp;($)</TableCell>
-    <TableCell align="left">Estado</TableCell>
-  </TableRow>
-</TableHead>
-<TableBody>
-  {rows.map((row) => (
-    <TableRow key={row.Id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-      <TableCell component="th" scope="row">{row.Hamburguesa}</TableCell>
-      <TableCell align="left">{row.Id}</TableCell>
-      <TableCell align="left">{row.Ventas}</TableCell>
-      <TableCell align="left">{row.Precio}</TableCell>
-      <TableCell align="left">
-        <span className="Estado" style={makeStyle(row.Estado)}>{row.Estado}</span>
-      </TableCell>
-      <TableCell align = 'left' className="Details">Details</TableCell>
-    </TableRow>
-  ))}
-</TableBody>
+            <TableRow>
+              <TableCell>Producto</TableCell>
+              <TableCell align="left">Id Orden</TableCell>
+              <TableCell align="left">Precio&nbsp;($)</TableCell>
+              <TableCell align="left">Dirección</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, idx) => (
+              <TableRow key={row.ID_Orden || idx} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">{row.Nombre_Plato}</TableCell>
+                <TableCell align="left">{row.ID_ORDEN}</TableCell>
+                <TableCell align="left">{row.PRECIO_PLATO}</TableCell>
+                <TableCell align="left">{row.DIRECCION_PEDIDO}</TableCell>
+
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
